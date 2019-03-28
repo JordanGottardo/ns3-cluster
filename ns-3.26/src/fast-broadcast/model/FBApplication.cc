fb50718277d82cd6049e70c45383262ff9acb325
JordanGottardo <jordangottardo@libero.it>
2019-03-28 10:28:47 +0100

    .

--- ns-3.26/src/fast-broadcast/model/FBApplication.cc
+++ ns-3.26/src/fast-broadcast/model/FBApplication.cc
@@ -275,8 +275,8 @@ void FBApplication::GenerateHelloTraffic(uint32_t count) {
 	NS_LOG_INFO(count);
 	NS_LOG_DEBUG("GenerateHelloTraffic " << count);
 	std::vector<int> he;
-	uint32_t hel = (int) m_nNodes / 100 * 50;		// 40% of total nodes
-//	uint32_t hel = (int) m_nNodes;		// 40% of total nodes
+//	uint32_t hel = (int) m_nNodes / 100 * 50;		// 40% of total nodes
+	uint32_t hel = (int) m_nNodes;		// 40% of total nodes
 	uint32_t time_factor = 10;
 //	cout << "hel= " << hel << endl;
 	if (count > 0)
