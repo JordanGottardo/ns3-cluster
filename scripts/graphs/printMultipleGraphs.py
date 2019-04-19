2517a7fa68206f66f7d3b9e0921c739c10e61526
JordanGottardo <jordangottardo@libero.it>
2019-04-19 14:43:43 +0200

    .

--- scripts/graphs/printMultipleGraphs.py
+++ scripts/graphs/printMultipleGraphs.py
@@ -164,8 +164,8 @@ def printAllComparison():
 	plt.rcParams["figure.figsize"] = [18, 10]
 	initialBasePath = "/home/jordan/MEGA/Universita_mia/Magistrale/Tesi/ns3-cluster/ns-3.26/out/scenario-urbano-con-coord"
 	#scenarios = ["Grid-200", "Grid-300", "Grid-400", "LA-15", "LA-25", "LA-35", "LA-45", "Padova-15", "Padova-25", "Padova-35", "Padova-45"]
-	scenarios = ["Platoon-15km"]
-	buildings = ["0", "1"]
+	scenarios = ["Grid-200"]
+	buildings = ["0"]
 	txRanges = ["100", "300", "500"]
 	protocols = ["Fast-Broadcast", "STATIC-100", "STATIC-300", "STATIC-500", "ROFF"]
 	#cws = ["cw[16-128]", "cw[32-1024]"]
