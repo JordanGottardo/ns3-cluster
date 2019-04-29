9ef1615b21f6b162802edd09133d8a0f3e9dc820
JordanGottardo <jordangottardo@libero.it>
2019-04-29 23:40:16 +0200

    .

--- scripts/generateMapsAndJobsTemplate.py
+++ scripts/generateMapsAndJobsTemplate.py
@@ -39,16 +39,16 @@ def createJobFile(newJobName, command, jobsPath, jobTemplatePath, tempNewJobPath
 def runScenario(cw, scenario, distance, startingNode, area=1000):
 	print(scenario)
 	# Protocols and transmission ranges
-	buildings = ["0"]
+	buildings = ["1"]
 	#errorRates = ["0", "10", "20", "30", "40", "50", "100"]
 	errorRates = ["0"]
 	forgedCoordRates = ["0", "10", "20", "30", "40", "50", "100"]
 	#buildings = ["1"]
 	junctions = ["0", "1"]
-	protocols = ["1", "2", "3", "4"]
-	#protocols = ["1", "2", "3", "4", "5"]
-	txRanges = ["100"]
-	#txRanges = ["100", 300", "500"]
+	#protocols = ["1", "2", "3", "4"]
+	protocols = ["1", "2", "3", "4", "5"]
+	#txRanges = ["100"]
+	txRanges = ["100", "300", "500"]
 	protocolsMap = {
 		"1": "Fast-Broadcast",
 		"2": "STATIC-100",
