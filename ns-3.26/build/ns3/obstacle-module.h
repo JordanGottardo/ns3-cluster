14939f86c98a3fd5fe5142ef0f053ba4a4918ae6
JordanGottardo <jordangottardo@libero.it>
2019-05-18 15:39:05 +0200

    .

---
+++ ns-3.26/build/ns3/obstacle-module.h
@@ -0,0 +1,14 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_OBSTACLE
+    
+
+// Module headers:
+#include "obstacle-helper.h"
+#include "obstacle-shadowing-propagation-loss-model.h"
+#include "obstacle.h"
+#include "topology.h"
+#endif
