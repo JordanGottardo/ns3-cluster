8e9547918d9a015634083f289f5aad5f79df7659
JordanGottardo <jordangottardo@libero.it>
2019-06-08 23:26:42 +0200

    .

--- scripts/generateMapsAndJobsTemplate.py
+++ scripts/generateMapsAndJobsTemplate.py
@@ -39,9 +39,9 @@ def createJobFile(newJobName, command, jobsPath, jobTemplatePath, tempNewJobPath
 def runScenario(cw, scenario, distance, startingNode, vehiclesNumber, area=1000):
 	print(scenario)
 	# Protocols and transmission ranges
-	highBuildings = ["0"]
+	highBuildings = ["0", "1"]
 	drones = ["0"]
-	buildings = ["0","1"]
+	buildings = ["1"]
 	#buildings = ["0"]
 	#errorRates = ["0", "10", "20", "30", "40", "50", "100"]
 	errorRates = ["0"]
@@ -49,7 +49,7 @@ def runScenario(cw, scenario, distance, startingNode, vehiclesNumber, area=1000)
 	#forgedCoordRates = ["0", "10", "20", "30", "40", "50", "100"]
 	#forgedCoordRates = ["0"]
 	#buildings = ["1"]
-	junctions = ["0", "1"]
+	junctions = ["0"]
 	#protocols = ["1", "2", "3", "4"]
 	protocols = ["1", "2", "3", "4", "5"]
 	#txRanges = ["100"]
@@ -143,7 +143,7 @@ def runScenario(cw, scenario, distance, startingNode, vehiclesNumber, area=1000)
 def main():
 	#Edit these to launch automatically 	forgedCoordRates = ["0", "10", "20", "30", "40", "50", "100"]
 
-	scenarios = ["Padova-25"]
+	scenarios = ["LA-25"]
 	#scenarios = ["Padova-5", "Padova-15", "Padova-25", "Padova-35", "Padova-45"] 
 	#scenarios = ["Padova-15", "Padova-25", "Padova-35", "Padova-45", "LA-15", "LA-25", "LA-35", "LA-45"]
 	#contentionWindows = [{"cwMin": 32, "cwMax": 1024}, {"cwMin": 16, "cwMax": 128}]
