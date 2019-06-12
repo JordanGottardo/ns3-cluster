81f8103248925d04b197e4237c1a22db55d865fa
JordanGottardo <jordangottardo@libero.it>
2019-06-12 09:37:02 +0200

    .

--- scripts/graphs/printMultipleGraphs.py
+++ scripts/graphs/printMultipleGraphs.py
@@ -358,7 +358,7 @@ def printProtocolComparison():
 	plt.rcParams["figure.figsize"] = [18, 6]
 	initialBasePath = "/home/jordan/MEGA/Universita_mia/Magistrale/Tesi/ns3-cluster/ns-3.26/out/scenario-urbano"
 	#scenarios = ["Grid-200", "Grid-300", "Grid-400", "LA-15", "LA-25", "LA-35", "LA-45", "Padova-15", "Padova-25", "Padova-35", "Padova-45"]
-	scenarios = ["LA-25"]
+	scenarios = ["Grid-300"]
 	buildings = ["0", "1"]
 	errorRate = "e0"
 	#txRanges = ["100", "300", "500"]
@@ -368,7 +368,7 @@ def printProtocolComparison():
 	cws = ["cw[32-1024]"]
 	#cws = ["cw[16-128]", "cw[32-1024]"]
 	#junctions = ["0", "1"]
-	junctions = ["0", "1"]
+	junctions = ["0"]
 	metrics = ["totCoverage", "covOnCirc", "hops", "slotsWaited", "messageSent"]
 	metricYLabels = {}
 	metricYLabels["totCoverage"] = "Total Delivery Ratio (%)"
