396992c99f59b47a68fd26e0d4c450306d805e8a
JordanGottardo <jordangottardo@libero.it>
2019-06-23 01:43:03 +0200

    .

--- scripts/generateMapsAndJobsTemplate.py
+++ scripts/generateMapsAndJobsTemplate.py
@@ -141,8 +141,8 @@ def runScenario(cw, scenario, distance, startingNode, vehiclesNumber, area=1000)
 
 def main():
 	#Edit these to launch automatically 	forgedCoordRates = ["0", "10", "20", "30", "40", "50", "100"]
-	#scenarios = ["Padova-5", "Padova-15", "Padova-25", "Padova-35", "Padova-45"]
-	scenarios = ["LA-25"]
+	scenarios = ["Padova-5", "Padova-15", "Padova-35", "Padova-45"]
+	#scenarios = ["LA-25"]
 	#scenarios = ["Padova-5", "Padova-15", "Padova-25", "Padova-35", "Padova-45"] 
 	#scenarios = ["Padova-15", "Padova-25", "Padova-35", "Padova-45", "LA-15", "LA-25", "LA-35", "LA-45"]
 	#contentionWindows = [{"cwMin": 32, "cwMax": 1024}, {"cwMin": 16, "cwMax": 128}]
